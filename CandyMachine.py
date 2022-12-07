f6a8fe3760777a063a3c8c54dc3158c7cfed742b
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-12-07 14:58:54 +0800

    Creating the Dispenser class

--- CandyMachine.py
+++ CandyMachine.py
@@ -16,3 +16,29 @@ class CashRegister:
     # Add the amount paid to the parameter
     def AcceptAmount(self, AmountPaid):
         self.CashOnHand += AmountPaid
+
+# Dispenser class
+
+    # Initializing the Parameter both with default value of 50
+    # If the parameter is less than 0, it will be assigned as 50
+    def __init__(self, NumberOfItems = 50, CostOfItems = 50):
+        if NumberOfItems < 0:
+            self.NumberOfItems = 50
+        else:
+            self.NumberOfItems = NumberOfItems
+        if CostOfItems < 0:
+            self.CostOfItems = 50
+        else:
+            self.CostOfItems = CostOfItems
+    
+    # returns the number of a particular product
+    def GetCount(self):
+        return self.NumberOfItems
+
+    # returms the cost of a product
+    def GetProductCost(self):
+        return self.CostOfItems
+
+    # when the product is sold, the product in dispenser is reduced by 1
+    def MakeSale(self):
+        self.NumberOfItems -= 1
