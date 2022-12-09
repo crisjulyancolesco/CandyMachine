d601acc0ce4c6ec0887e61f30a8b89286aa44d40
crisjulyancolesco <crisjulyancolesco@iskolarngbayan.pup.edu.ph>
2022-12-09 22:51:23 +0800

    Final running output

--- CandyMachineGUI.py
+++ CandyMachineGUI.py
@@ -1,72 +1,258 @@
-# Importing tkinter
 from tkinter import *
 
-# Creating the window
-window1 = Tk()
-window1.title("Cris Candy Machine")
-window1.geometry("600x400+10+20")
+# Function for Cancel button in 2nd window
+def Cancel():
+    return StartProgram()
 
-# Functions for each buttons
-def Candy():
+# Function for Okay button in 3rd window
+def Okay():
+    return StartProgram()
 
-    # Creating string variable
-    StrVar = StringVar()
-
-    # Creating new window with label
-    window2 = Tk()
-    window2.title("Candy")
-    window2.geometry("600x400+10+20")
-
-    def Cancel():
-        window1.mainloop()
-
-    def Okay():
-        window3 = Tk()
-        window3.title("Candy")
-        window3.geometry("600x400+10+20")
-
-        def Okay2():
-            return # not yet finished
-
-        canvas = Canvas(window3, width= 500, height= 10)
-        canvas.pack(side = TOP, pady = 10) 
-
-        Label(window3, text = 'Please pick up your Candy and enjoy!!!', font=' Helvetica 10').pack(side=TOP)
-
-        Button(window3,width= 10, text="Okay",font="Helveica 10", command = Okay2).place(x = 180, y = 105)
-
-        window3.mainloop()
+# Function for Okay button in Candy
+def Okay1():
+    
+    # Create the 3rd window
+    window = Tk()
+    window.title("Thank you, Come Again!!!")
+    window.geometry("600x400+10+20")
 
     # Blank space
-    canvas = Canvas(window2, width= 500, height= 10)
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label and Button for 3rd window
+    Label(window, text = 'Please pick up your Candy and enjoy!!!', font=' Helvetica 10').pack(side=TOP)
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay).place(x = 180, y = 105)
+
+    window.mainloop()
+
+# Function for Okay button in Chips
+def Okay2():
+    
+    # Create the 3rd window
+    window = Tk()
+    window.title("Thank you, Come Again!!!")
+    window.geometry("600x400+10+20")
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label and Button for 3rd window
+    Label(window, text = 'Please pick up your Chips and enjoy!!!', font=' Helvetica 10').pack(side=TOP)
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay).place(x = 180, y = 105)
+
+    window.mainloop()
+
+# Function for Okay button in Gum
+def Okay3():
+    
+    # Create the 3rd window
+    window = Tk()
+    window.title("Thank you, Come Again!!!")
+    window.geometry("600x400+10+20")
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label and Button for 3rd window
+    Label(window, text = 'Please pick up your Gum and enjoy!!!', font=' Helvetica 10').pack(side=TOP)
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay).place(x = 180, y = 105)
+
+    window.mainloop()
+
+# Function for Okay button in Cookies
+def Okay4():
+    
+    # Create the 3rd window
+    window = Tk()
+    window.title("Thank you, Come Again!!!")
+    window.geometry("600x400+10+20")
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label and Button for 3rd window
+    Label(window, text = 'Please pick up your Cookies and enjoy!!!', font=' Helvetica 10').pack(side=TOP)
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay).place(x = 180, y = 105)
+
+    window.mainloop()
+
+# Function for Candy button
+def Candy():
+   
+    # Creating integer variable
+    Payment = IntVar()
+
+    # Creating 2nd window 
+    window = Tk()
+    window.title("Candy")
+    window.geometry("600x400+10+20")   
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
     canvas.pack(side = TOP, pady = 10) 
 
     # Label for the instruction
-    Label(window2, text = 'To buy Candy, please insert 10 cents:', font=' Helvetica 10').pack(side=TOP)
+    Label(window, text = 'To buy Candy, please insert 10 cents:', font=' Helvetica 10').pack(side=TOP)
 
     # Creating the frame where the user can type
-    Entry(window2, textvariable = StrVar,width=60).pack(pady = 10)
+    Entry(window, textvariable = Payment,width=60,).pack(pady = 10)
 
     # Creating the Ok and Cancel button
-    Button(window2,width= 10, text="Okay",font="Helveica 10", command = Okay).place(x = 180, y = 105)
-    Button(window2,width= 10, text="Cancel",font="Helveica 10", command = Cancel).place(x = 320, y = 105)
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay1).place(x = 180, y = 105)
+    Button(window,width= 10, text="Cancel",font="Helveica 10", command = Cancel).place(x = 320, y = 105)
 
-    window2.mainloop()
+    window.mainloop()
+
+# Function for Chips button
+def Chips():
+   
+    # Creating integer variable
+    Payment = IntVar()
+
+    # Creating 2nd window 
+    window = Tk()
+    window.title("Chips")
+    window.geometry("600x400+10+20")   
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label for the instruction
+    Label(window, text = 'To buy Chips, please insert 50 cents:', font=' Helvetica 10').pack(side=TOP)
+
+    # Creating the frame where the user can type
+    Entry(window, textvariable = Payment,width=60,).pack(pady = 10)
+
+    # Creating the Ok and Cancel button
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay2).place(x = 180, y = 105)
+    Button(window,width= 10, text="Cancel",font="Helveica 10", command = Cancel).place(x = 320, y = 105)
+
+    window.mainloop()
+
+# Function for Gum button
+def Gum():
+   
+    # Creating integer variable
+    Payment = IntVar()
+
+    # Creating 2nd window 
+    window = Tk()
+    window.title("Gum")
+    window.geometry("600x400+10+20")   
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label for the instruction
+    Label(window, text = 'To buy Gum, please insert 25 cents:', font=' Helvetica 10').pack(side=TOP)
+
+    # Creating the frame where the user can type
+    Entry(window, textvariable = Payment,width=60,).pack(pady = 10)
+
+    # Creating the Ok and Cancel button
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay3).place(x = 180, y = 105)
+    Button(window,width= 10, text="Cancel",font="Helveica 10", command = Cancel).place(x = 320, y = 105)
+
+    window.mainloop()
+
+# Function for Gum button
+def Cookies():
+
+    # Creating integer variable
+    Payment = IntVar()
+
+    # Creating 2nd window
+    
+    window = Tk()
+    window.title("Cookies")
+    window.geometry("600x400+10+20")   
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label for the instruction
+    Label(window, text = 'To buy Cookies, please insert 90 cents:', font=' Helvetica 10').pack(side=TOP)
+
+    # Creating the frame where the user can type
+    Entry(window, textvariable = Payment,width=60,).pack(pady = 10)
+
+    # Creating the Ok and Cancel button
+    Button(window,width= 10, text="Okay",font="Helveica 10", command = Okay4).place(x = 180, y = 105)
+    Button(window,width= 10, text="Cancel",font="Helveica 10", command = Cancel).place(x = 320, y = 105)
+
+    window.mainloop()
+
+# Function for Exit button
+def Exit():
+
+    # Create the 2nd window
+    window = Tk()
+    window.title("Thank you, Come Again!!!")
+    window.geometry("600x400+10+20")
+
+    # Blank space
+    canvas = Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10) 
+
+    # Label and Button for 3rd window
+    Label(window, text = 'Thank you for you purchase!!', font=' Helvetica 10').pack(side=TOP, pady = 10)
+    Label(window, text = "***** Cris' Candy Machine *****", font=' Helvetica 10').pack(side=TOP)
+
+    window.mainloop()
+
+# Function to start the program again
+def StartProgram():
+    # Creating the 1st window
+    window = Tk()
+    window.title("Cris Candy Machine")
+    window.geometry("600x400+10+20")
+
+    # Blank space
+    canvas= Canvas(window, width= 500, height= 10)
+    canvas.pack(side = TOP, pady = 10)
+
+    # For the shop name
+    Button(window, width= 60, text="WELCOME TO CRIS' SWEET CANDY SHOP", font=('Helvetica 10 bold'), fg = 'red').pack(side=TOP, pady = 5)
+
+    # Label for the Instruction
+    Label(window, text = 'To make a selection, Click on the Product Button Below:', font=' Helvetica 10').pack(side=TOP, pady = 20)
+
+    # Buttons
+    Button(window,width= 45, text="Candy",font="Helveica 10", command = Candy).pack(side=TOP, pady = 3)
+    Button(window,width= 45, text="Chips",font="Helveica 10", command = Chips).pack(side=TOP, pady = 3)
+    Button(window,width= 45, text="Gum",font="Helveica 10", command = Gum).pack(side=TOP, pady = 3)
+    Button(window,width= 45, text="Cookies",font="Helveica 10", command = Cookies).pack(side=TOP, pady = 3)
+    Button(window,width= 45, text="Exit",font="Helveica 10", command = Exit).pack(side=TOP, pady = 3)
+
+    window.mainloop()
+
+# Creating the 1st window
+window = Tk()
+window.title("Cris Candy Machine")
+window.geometry("600x400+10+20")
+
 # Blank space
 canvas= Canvas(width= 500, height= 10)
 canvas.pack(side = TOP, pady = 10)
 
 # For the shop name
-Button(window1, width= 60, text="WELCOME TO CRIS' SWEET CANDY SHOP", font=('Helvetica 10 bold'), fg = 'red').pack(side=TOP, pady = 5)
+Button(window, width= 60, text="WELCOME TO CRIS' SWEET CANDY SHOP", font=('Helvetica 10 bold'), fg = 'red').pack(side=TOP, pady = 5)
 
 # Label for the Instruction
 Label(text = 'To make a selection, Click on the Product Button Below:', font=' Helvetica 10').pack(side=TOP, pady = 20)
 
 # Buttons
-Button(window1,width= 45, text="Candy",font="Helveica 10", command = Candy).pack(side=TOP, pady = 3)
-Button(window1,width= 45, text="Chips",font="Helveica 10").pack(side=TOP, pady = 3)
-Button(window1,width= 45, text="Gum",font="Helveica 10").pack(side=TOP, pady = 3)
-Button(window1,width= 45, text="Cookies",font="Helveica 10").pack(side=TOP, pady = 3)
-Button(window1,width= 45, text="Exit",font="Helveica 10").pack(side=TOP, pady = 3)
+Button(window,width= 45, text="Candy",font="Helveica 10", command = Candy).pack(side=TOP, pady = 3)
+Button(window,width= 45, text="Chips",font="Helveica 10", command = Chips).pack(side=TOP, pady = 3)
+Button(window,width= 45, text="Gum",font="Helveica 10", command = Gum).pack(side=TOP, pady = 3)
+Button(window,width= 45, text="Cookies",font="Helveica 10", command = Cookies).pack(side=TOP, pady = 3)
+Button(window,width= 45, text="Exit",font="Helveica 10", command = Exit).pack(side=TOP, pady = 3)
 
-window1.mainloop()
+window.mainloop()
